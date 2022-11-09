d35fe85b12b674ae94464d49520f14a26d38f862
Hasbleidy <110091586+hlindmart@users.noreply.github.com>
2022-11-09 14:41:26 -0600

    python work 1

---
+++ main.py
@@ -0,0 +1,36 @@
+#create three variables set to the following questions
+nameQ="What is your name?"
+ageQ="How old are you?"
+monthsQ="How many months is that?"
+
+#create three variables answer the above questions
+nameA='My name is "Billy".'
+ageA=30
+monthsA=360
+
+#print each of the variables
+print(nameQ)
+print(nameA)
+print(ageQ)
+print(ageA)
+print(monthsQ)
+print(monthsA)
+
+#comment out the appropriate variables
+print(nameQ)
+print(nameA)
+print(ageQ)
+print(ageA)
+# print(monthsQ)
+# print(monthsA)
+
+#create three new variables of string, integer, and float and print them on a separate line
+my_string="A bowl of cereal, some eggs, and bacon make a great breakfast."
+my_integer=29
+my_float=77.888
+
+print(my_string)
+
+print(my_integer)
+
+print(my_float)
